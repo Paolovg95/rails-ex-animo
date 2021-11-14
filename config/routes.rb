a2f99518f70cd7efd5b1d386c51d30b2d1ace8b5
Paolo Vargas <paolo9517@gmail.com>
2021-11-14 18:02:35 -0300

    Registrations and Login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'providers/registrations'
-  }
+  devise_for :users
+  devise_for :shelters
+  devise_for :providers
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
