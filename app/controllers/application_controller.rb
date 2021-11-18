1792b737c24ab1f0dad76ab229a803dfcd20c215
Paolo Vargas <paolo9517@gmail.com>
2021-11-18 11:05:50 -0300

    Navbar View based on which User is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :address, :phone_number, :category)}    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :address, :phone_number, :category)}    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
